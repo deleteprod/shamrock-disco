a1b41853287969dc75a67fbd83d4885f2afd3ddb
Martyn O'Connor <contact@martynoconnor.net>
2017-03-06 21:55:26 +0000

    Update ryanair_api_pull.py
    Added the ability to use command line switches rather than hardcoded variables
    Tidied up the date and time formatting in the results to be more easily human readable.

--- ryanair_api_pull.py
+++ ryanair_api_pull.py
@@ -4,45 +4,45 @@
 import json
 import urllib2
 import datetime
+import argparse
 
-""" To do:  learn more about argparse module to make the script more versatile
-Specifically, use argparse to allow passing of specific source airports, price
-limits and dates etc
-"""
+parser = argparse.ArgumentParser(description='Shamrock Disco - the Ryanair Fare Finder')
+parser.add_argument('-o','--origin', help='IATA code for airport of origin', required=True)
+parser.add_argument('-w','--window', help='Time window for search in days', required=True)
+parser.add_argument('-m','--maxprice', help='Maximum price you want to pay', required=False)
+args = vars(parser.parse_args())
 
-# Get dates in raw format to allow for datetime.timedelta functions
+# Get date in raw format to allow for datetime.timedelta function
 today_raw = datetime.datetime.today()
-next_week_raw = today_raw + datetime.timedelta(days=7)
-next_month_raw = today_raw + datetime.timedelta (days=30)
 
-# Convert those into something the JSON API will like
+# Accept the window parameter from argparse and make it an integer for timedelta
+window=int(args['window'])
+date_to = today_raw + datetime.timedelta(days=window)
+
+# Convert dates into something the JSON API will take
 today_formatted = today_raw.strftime('%Y-%m-%d')
-next_week_formatted = next_week_raw.strftime('%Y-%m-%d')
-next_month_formatted = next_month_raw.strftime('%Y-%m-%d')
+date_limit = date_to.strftime('%Y-%m-%d')
 
-# Other variables for the API call - should we do it this way?
-airport_code = "BHX" # Birmingham. Choose departure airport here.
-date_limit = next_month_formatted # Choose the date limit here
-price_limit = 20 # Price limit for single fare, in Euros. See jsonrates.com for next version converting to/from pounds
+# Other variables for the API call
+airport_code = args['origin']
+price_limit = args['maxprice'] # Price limit for single fare
 
-# For now - define the url for the JSON API
+# Define the url for the JSON API
 url = "https://api.ryanair.com/farefinder/3/oneWayFares?&departureAirportIataCode=%s&language=en&limit=30&market=en-gb&offset=0&outboundDepartureDateFrom=%s&outboundDepartureDateTo=%s&priceValueTo=%s" % (airport_code, today_formatted, date_limit, price_limit)
 
 # Define how we will format the results coming back
-
 def printResults(data):
     theJSON = json.loads(data)
 
     # for each result brought back by the JSON API query, extract relevant data
     for i in theJSON["fares"]:
+        departure_date = str(i["outbound"]["departureDate"]) # string the departure date
+        the_date = datetime.datetime.strptime(departure_date, "%Y-%m-%dT%H:%M:%S").strftime("%d %B - %H:%M") # tidy up the date
         print "From "  + i["outbound"]["departureAirport"]["seoName"].upper() + " to  " +  i["outbound"]["arrivalAirport"]["seoName"].upper()
-        print "On " + str(i["outbound"]["departureDate"]) + " for " + str(i["outbound"]["price"]["value"])
-        print '\n'
+        print "On " + the_date + " for " + str(i["outbound"]["price"]["value"])
 
 # use urllib2 to open the URL and output it to variable named data
-
 webUrl = urllib2.urlopen(url)
-print webUrl.getcode()
 if (webUrl.getcode() == 200): # Check that we got a 200 HTTP response
     data = webUrl.read()
     printResults(data)
